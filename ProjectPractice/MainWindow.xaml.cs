85402042fad044a27055e49a4b39c6321b9e22c1
Sense2015 <tsujin@senseet.com>
2015-10-06 11:36:52 +0800

    圖片加入完成

--- ProjectPractice/MainWindow.xaml.cs
+++ ProjectPractice/MainWindow.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Configuration;
 using System.IO;
+using System.Diagnostics;
+using System.Drawing;
 
 namespace ProjectPractice
 {
@@ -25,7 +27,10 @@ namespace ProjectPractice
     {
         string line;
         string[] words;
-        int x, y;
+        int x, y,countimg;
+        string[] imagePaths;
+        string imageFolder;
+        ImageBrush berriesBrush;
         public MainWindow()
         {
             InitializeComponent();
@@ -41,28 +46,27 @@ namespace ProjectPractice
             //讀txt
             StreamReader sr = new StreamReader(File.OpenRead(@"C:\Sense2015\ProjectPractice\ProjectPractice\formxy.txt"));
             while (!sr.EndOfStream) {               // 每次讀取一行，直到檔尾
-                
                 line = sr.ReadLine();            // 讀取文字到 line 變數
                 words = line.Split(',');        //切割完存到words
             }
             sr.Close();                     // 關閉串流
-            //for(int i= 0 ; i<words.Length;i++){
-            //    MessageBox.Show(words[i]);  //測試用
-            //}
-
+            
             //建立快捷鍵ESC
+            
             InputBinding ib = new InputBinding(ApplicationCommands.Properties, new KeyGesture(Key.Escape));
             this.InputBindings.Add(ib);
+            
             CommandBinding cb = new CommandBinding(ApplicationCommands.Properties);
             cb.Executed += new ExecutedRoutedEventHandler(cb_Executed);
             this.CommandBindings.Add(cb);
+            
         }
         //聽到ESC之後關程式
         void cb_Executed(object sender, ExecutedRoutedEventArgs e)
         {
             Application.Current.Shutdown();
         }
-        //public bool CanExecute(object parameter)ㄋ
+        //public bool CanExecute(object parameter)
         //{
         //    //we can only close Windows
         //    return (parameter is Window);
@@ -77,11 +81,7 @@ namespace ProjectPractice
         //        ((Window)parameter).Close();
         //    }
         //}
-        private void button1_Click(object sender, RoutedEventArgs e)
-        {
-            CreateButton(6, 6);
-        }
-
+        
 
         private void button2_Click(object sender, RoutedEventArgs e)
         {
@@ -93,21 +93,12 @@ namespace ProjectPractice
         private void CreateButton(int x, int y)
         {
 
-
-
             canvas1.Children.Clear();
             
-            //double w= (this.canvas1.ActualWidth - (x + 1) * 5) / x; 
-            ////MessageBox.Show("" + w);
-            //double h = (this.canvas1.ActualHeight - (x + 1) * 5) / x; 
-            ////MessageBox.Show("" + h);
-            
-            //四个方向的边距都是5
             double width = (this.canvas1.ActualWidth - (x + 1) * 5) / x; //按鈕寬
             double height = (this.canvas1.ActualHeight - (y + 1) * 5) / y; //按鈕高
-
-
             
+            countimg = 0;
 
             for (int i = 0; i < x; i++)
             {
@@ -117,23 +108,81 @@ namespace ProjectPractice
                     {
                         Width = width,
                         Height = height,
-                        //Background=new Image(C:\Sense2015\ProjectPractice\ProjectPractice\images\backstep.png)
                     };
 
-                    //MessageBox.Show("" + i + "," + j); //test
-
-                    Canvas.SetTop(bt, j * height + 5);
+                    if (countimg < imagePaths.Length)
+                    {
+                        MessageBox.Show("" + imagePaths[countimg]);
+                        berriesBrush = new ImageBrush();
+                        berriesBrush.ImageSource = new BitmapImage(new Uri(@"" + imagePaths[countimg], UriKind.Relative));
+                        countimg++;
+                    }
+                    
+                    bt.Background = berriesBrush;
+                    
+                    Canvas.SetTop(bt, j * height + 5);  //建立間距
                     Canvas.SetLeft(bt, i * width + 5);
-                    //这两句很关键。按钮在Canvas中的定位与它自己的Left以及Top不是一个概念
-
                     
                     canvas1.Children.Add(bt);
-
-
                 }
             }
+        }
+        // 先將圖檔讀到FileStream, 再轉換為byte array。
+        private static void ToBinaryByFileStream(string imageFile)
+        {
+            FileStream fs = new FileStream(imageFile, FileMode.Open);
+            byte[] buffer = new byte[fs.Length];
+            fs.Read(buffer, 0, buffer.Length);
+            fs.Close();
+        }
 
+        // 先將圖檔讀到Image object, 再轉換為byte array。
+        //private static void ToBinaryByImageObj(string imageFile)
+        //{
+        //    System.Drawing.Image image = System.Drawing.Image.FromFile(imageFile);
+        //    MemoryStream mr = new MemoryStream();
+        //    image.Save(mr, System.Drawing.Imaging.ImageFormat.Png);
+        //    byte[] binaryImage = mr.ToArray();
+        //    mr.Dispose();
+        //    image.Dispose();
+        //}
+        //public static void ConvertImageToBinary()
 
+        //將資料夾中的圖片"路徑"存到imagepaths
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            //Console.WriteLine("=== Convert the image file to the binary object. ===");
+
+            imageFolder = @"C:\Sense2015\ProjectPractice\ProjectPractice\images";
+            // 測試的image數量。
+            //int sampleCount = 14;
+
+            imagePaths = Directory.GetFiles(imageFolder, "*.png");
+            if (0 == imagePaths.Length)
+            {
+                MessageBox.Show("No image found.");
+                return;
+            }
+
+            //Stopwatch sw = new Stopwatch();
+
+            //sw.Reset();
+            //sw.Start();
+            //for (int i = 0; i < sampleCount; i++)
+            //{
+            //    ToBinaryByFileStream(imagePaths[i]);
+            //}
+            //sw.Stop();
+            //Console.WriteLine("By FileStream: {0} ms.",sw.ElapsedMilliseconds);
+
+            //sw.Reset();
+            //sw.Start();
+            ////for (int i = 0; i < sampleCount; i++)
+            ////{
+            ////    ToBinaryByImageObj(imagePaths[i]);
+            ////}
+            //sw.Stop();
+            //Console.WriteLine("By System.Drawing.Image: {0} ms.",sw.ElapsedMilliseconds);
         }
     }
 }
